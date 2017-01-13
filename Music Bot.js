50d879ae3b00b11c7a33440a861907deec96a851
JhonforgeGaming <2boyslovemom@gmail.com>
2017-01-13 14:30:26 -0600

    changed `console.log` placement

--- Music Bot.js
+++ Music Bot.js
@@ -8,7 +8,7 @@ let prefix = "!";
 const voiceChannel = msg.member.voiceChannel;
 
 	
-	console.log("Shutdown...")
+	console.log("Shutdown?")
 	if (msg.content.startsWith(prefix + "shutdownbot")) {
 		if (msg.author.id != "235144525763444736") return;
 		console.log(`Shutdown initiated by ${msg.author}...`)
@@ -16,96 +16,97 @@ const voiceChannel = msg.member.voiceChannel;
 		process.exit()
 	}
 	
-	console.log("Playing Echo.mp3")
 	if (msg.content.startsWith(prefix + "playEcho")) {
+		console.log("Playing Echo.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/ECHO.mp3');
 		})
 	}
 	
-	console.log("Playing Karma.mp3")
 	if (msg.content.startsWith(prefix + "playKarma")) {
+		console.log("Playing Karma.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/KARMA.mp3');
 		})
 	}
 	
-	console.log("Playing OMFG.mp3")
+	
 	if (msg.content.startsWith(prefix + "playOMFG")) {
+		console.log("Playing OMFG.mp3")
 		voiceChannel.join()
 		.then(connection=> {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/OMFG.mp3');
 		})
 	}
 	
-	console.log("Playing Exorcism.mp3")
 	if (msg.content.startsWith(prefix + "playExorcism")) {
+		console.log("Playing Exorcism.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Exorcism.mp3');
 		})
 	}
 	
-	console.log("Playing Timberwolf.mp3")
 	if (msg.content.startsWith(prefix + "playTimberwolf")) {
+		console.log("Playing Timberwolf.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Timberwolf.mp3');
 		})
 	}
 	
-		console.log("Playing Tetris.mp3")
 	if (msg.content.startsWith(prefix + "playTetris")) {
+		console.log("Playing Tetris.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Tetris.mp3');
 		})
 	}
 	
-		console.log("Playing Breathe Out.mp3")
 	if (msg.content.startsWith(prefix + "playBreathe")) {
+		console.log("Playing Breathe Out.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Breathe.mp3');
 		})
 	}
 	
-			console.log("Playing Middle.mp3")
 	if (msg.content.startsWith(prefix + "playMiddle")) {
+		console.log("Playing Middle.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Middle.mp3');
 		})
 	}
 	
-			console.log("Playing Stand.mp3")
 	if (msg.content.startsWith(prefix + "playStand")) {
+		console.log("Playing Stand.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Stand.mp3');
 		})
 	}
 	
-			console.log("Playing Wildfire.mp3")
 	if (msg.content.startsWith(prefix + "playWildfire")) {
+		console.log("Playing Wildfire.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Wildfire.mp3');
 		})
 	}
 	
-			console.log("Playing Stars.mp3")
 	if (msg.content.startsWith(prefix + "playStars")) {
+		console.log("Playing Stars.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Stars.mp3');
 		})
 	}
 	
-			console.log("Playing Asthma.mp3")
 	if (msg.content.startsWith(prefix + "playAsthma")) {
+		console.log("Playing Asthma.mp3")
 		voiceChannel.join()
 		.then(connection => {
 			let dispatcher = connection.playFile('C:/Users/Jhonforge_Gaming/Music/Asthma.mp3');
